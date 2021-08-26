bc31130c38ad0ced118ab4eeb98e26d8c8872512
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-26 22:23:13 +0200

    Added initial modal card holder for homepage display

--- client/src/Event/Home.jsx
+++ client/src/Event/Home.jsx
@@ -1,14 +1,59 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-
+import './Home.css';
+import Card from '../components/Card/Card';
 
 //We are taking in function and returning our state
 const Home = () => {
     const {user} = useSelector((state) => ({...state}))  
     return (
         <div className="container-fluid h1 p-5 text-center">
-            Event List Home{JSON.stringify(user)}
+            Home Component {JSON.stringify(user)} 
+            <div className='home'>
+
+            <div className='home__section'>
+            <Card
+                 src="https://unsplash.com/photos/_SUo0bFBMRc"
+                 title="Seeking Artist For Open Calls"
+                 description="Gutarist And All Musicians are welcomed"
+                 price="Monthly Gig at $1000"
+            />
+            <Card
+                src="https://unsplash.com/photos/_SUo0bFBMRc"
+                title="Seeking Artist For Open Calls"
+                description="Gutarist And All Musicians are welcomed"
+                price="Monthly Gig at $1000"
+            />
+            <Card
+                 src="https://unsplash.com/photos/_SUo0bFBMRc"
+                 title="Seeking Artist For Open Calls"
+                 description="Gutarist And All Musicians are welcomed"
+                 price="Monthly Gig at $1000"
+            />
+            </div>
+            <div className='home__section'>
+            <Card
+                 src="https://unsplash.com/photos/_SUo0bFBMRc"
+                 title="Seeking Artist For Open Calls"
+                 description="Gutarist And All Musicians are welcomed"
+                 price="Monthly Gig at $1000"
+            />
+            <Card
+                src="https://unsplash.com/photos/_SUo0bFBMRc"
+                title="Seeking Artist For Open Calls"
+                description="Gutarist And All Musicians are welcomed"
+                price="Monthly Gig at $1000"
+            />
+            <Card
+                src="https://unsplash.com/photos/_SUo0bFBMRc"
+                title="Seeking Artist For Open Calls"
+                description="Gutarist And All Musicians are welcomed"
+                price="Monthly Gig at $1000"
+            />
+            </div>
         </div>
+        </div>
+        
     )
 };
 
