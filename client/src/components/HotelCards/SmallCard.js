ebe369ac82ca39334cd1a4ee7d605ef2782e6189
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-09-03 00:45:57 +0200

    Added the card body display for hotel properties display

--- client/src/components/HotelCards/SmallCard.js
+++ client/src/components/HotelCards/SmallCard.js
@@ -1,5 +1,28 @@
 const SmallCard = ({ h }) => {
-  <>{JSON.stringify(h)}</>;
+  <>
+    <div className="card">
+      <div className="card__info">
+        <div className="card mb-3">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img
+                src="https://source.unsplash.com/user/erondu/900x500.png?text=Muse+Connect"
+                alt="default pic Hotel"
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">
+                  {h.title}
+                  <span className="float-right">$0.00</span>
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>;
 };
 
 export default SmallCard;
